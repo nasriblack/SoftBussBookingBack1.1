251c918212fa26dbbd08629dac203dcb9737e7c6
Nasereddine Lakhal <naserlakhal@gmail.com>
2025-05-11 12:38:03 +0100

    add clear database cmd

---
+++ prisma/clear_database.ts
@@ -0,0 +1,19 @@
+import { prismaClient } from "./../src/utils/prisma";
+
+async function deleteDatabase() {
+  // Delete in proper order to avoid FK constraint violations
+  await prismaClient.reservation.deleteMany();
+  await prismaClient.seat.deleteMany();
+  await prismaClient.bus.deleteMany();
+  await prismaClient.user.deleteMany();
+  await prismaClient.whiteListUser.deleteMany();
+}
+
+deleteDatabase()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prismaClient.$disconnect();
+  });
