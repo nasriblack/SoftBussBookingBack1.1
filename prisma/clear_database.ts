11b50a9d3be09845f9c1fcacb5e678940f59ac33
Nasereddine Lakhal <naserlakhal@gmail.com>
2025-05-11 14:29:53 +0100

    update the clear database code

--- prisma/clear_database.ts
+++ prisma/clear_database.ts
@@ -3,7 +3,6 @@ import { prismaClient } from "./../src/utils/prisma";
 async function deleteDatabase() {
   // Delete in proper order to avoid FK constraint violations
   await prismaClient.reservation.deleteMany();
-  await prismaClient.seat.deleteMany();
   await prismaClient.bus.deleteMany();
   await prismaClient.user.deleteMany();
   await prismaClient.whiteListUser.deleteMany();
