58aba672ac1f389bf13637e72ec9639bd6846fcb
Nasereddine Lakhal <naserlakhal@gmail.com>
2025-06-07 16:03:56 +0100

    making an example on the websocket

--- README.md
+++ README.md
@@ -46,6 +46,7 @@
   -[ ] Add const varaible
 
 - [ ] Find a github project using socket.IO 
+- [ ] Make an example using socket.IO => new branch 
 
 
 
@@ -145,6 +146,7 @@ https://socket.io/docs/v4/
 - we receive a msg with the on and we tell the type of the msg
   - for example we emit a msg with ping we should receive this by ping socket.on('ping')
 - first connection is establish using the io.on connection event ! ( the io is our server )
+- every connection of user have his own socket.id => 
 
 Sender
 ```js
--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,31 @@ import UserRouter from "./routes/user.routes";
 import { errorHandler } from "./middleware/errorHandler";
 import { notFoundHandler } from "./middleware/not-found";
 import cookieParser from "cookie-parser";
+import http from "http";
+import { Server } from "socket.io";
+import path from "path";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/ws", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+// Socket.IO handling
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+  socket.join("room1");
+  socket.on("chat message", (msg) => {
+    console.log("Message from client:", msg);
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+  });
+});
+
 app.disable("x-powered-by");
 app.use(morgan("dev"));
 app.use(urlencoded({ extended: true }));
@@ -22,4 +45,4 @@ app.use(apiVersion, UserRouter);
 app.use(errorHandler);
 app.use(notFoundHandler);
 
-export default app;
+export default server;
