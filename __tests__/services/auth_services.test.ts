4c937c5fffde20364e13558912a6028c277bd8a9
Nasereddine Lakhal <naserlakhal@gmail.com>
2025-06-23 19:37:38 +0100

    refactore the code to check the middleware register

--- __tests__/services/auth_services.test.ts
+++ __tests__/services/auth_services.test.ts
@@ -2,6 +2,7 @@ import { type Express } from "express";
 import request from "supertest";
 import { createServer } from "../../src/app";
 import { apiVersion, endPoint } from "../../src/utils/endpoints";
+import HttpStatusCode from "../../src/utils/httpStatusCode";
 
 describe("Test Auth Services", () => {
   describe("Test the register service", () => {
@@ -29,7 +30,6 @@ describe("Test Auth Services", () => {
         .post(`${apiVersion}${endPoint.Authentification.REGISTRATION}`)
         .send(user)
         .expect(404);
-      console.log("checking the response", response);
     });
     test("should throw an error when register with empty email", async () => {
       const user = {
@@ -37,10 +37,10 @@ describe("Test Auth Services", () => {
         password: "nasri",
       };
 
-      const response = await request(createServer().app)
+      await request(createServer().app)
         .post(`${apiVersion}${endPoint.Authentification.REGISTRATION}`)
         .send(user)
-        .expect(400);
+        .expect(HttpStatusCode.BAD_REQUEST);
     });
     test("should throw an error when register with empty password", async () => {
       const user = {
@@ -48,14 +48,27 @@ describe("Test Auth Services", () => {
         password: "",
       };
 
-      await request(createServer().app)
+      const response = await request(createServer().app)
         .post(`${apiVersion}${endPoint.Authentification.REGISTRATION}`)
         .send(user)
         .expect(404);
+      expect(response.body.error.message as string).toBe("User Already Exist");
     });
   });
 
-  describe("Test the login service", () => {
-    test("should passe the login", () => {});
+  describe.skip("Test the login service", () => {
+    test("should passe the login", async () => {
+      const user = {
+        email: "nasereddine.lakhal@blacode.com",
+        password: "nasri",
+      };
+
+      await request(createServer().app)
+        .post(`${apiVersion}${endPoint.Authentification.LOGIN}`)
+        .send(user)
+        .expect(404);
+    });
+    test("should throw an error with an empty email", () => {});
+    test("should throw an error with an empty password", () => {});
   });
 });
