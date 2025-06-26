7f974510c07aea541331ba6ec3bb7e275373945f
Nasereddine Lakhal <naserlakhal@gmail.com>
2025-06-26 15:56:49 +0100

    all test passed for the create reservartion

--- __tests__/services/reservation_service.test.ts
+++ __tests__/services/reservation_service.test.ts
@@ -9,6 +9,7 @@ describe("Test the Reservation Service", () => {
   let userId = "";
 
   describe("should create a reservation", () => {
+    // ALL TESTS PASSED
     test.skip("should create a reservation", async () => {
       let userId = "796c0c01-afae-4c63-84b0-272932dd7f82";
       let token =
@@ -26,10 +27,8 @@ describe("Test the Reservation Service", () => {
         .post(`${apiVersion}${endPoint.Reservation.ADD_RESERVATION}`)
         .set("Cookie", `jwt=${token}`)
         .send(body)
-        // .expect(201)
-        .then((res) => {
-          console.log("Response:", res);
-        });
+
+        .expect(201);
     });
 
     test("should throw an error when creating a reservation for a non authentificated user", async () => {
@@ -77,15 +76,34 @@ describe("Test the Reservation Service", () => {
       expect(response.body.error.message as string).toBe("User Not Found");
     });
 
-    test("should throw an error when creating a reservation with invalid data", async () => {
-      // Test implementation
-    });
-
     test("should throw an error when creating a reservation with an existing seat", async () => {
       // Test implementation
+
+      let userId = "796c0c01-afae-4c63-84b0-272932dd7f82";
+      let token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoibmFzZXJlZGRpbmUubGFraGFsQGJsYWNvZGUuY29tIiwiaWQiOiI3OTZjMGMwMS1hZmFlLTRjNjMtODRiMC0yNzI5MzJkZDdmODIiLCJpc1ZlcmlmaWVkIjpmYWxzZSwicmVzZXJ2YXRpb25zIjpbXSwicm9sZSI6IlVTRVIiLCJwYXNzd29yZCI6IiQyYiQxMCQ1NHVreVhIZ2dmdGNTRDNDR2poeUxPL0hmczM5N2NTZ1JYODlsVEdoRURsb0hHMzBDT1A3ZSJ9LCJpYXQiOjE3NTA5NDAxOTIsImV4cCI6MTc1MTAyNjU5Mn0.1vVTWFfk185LftWa-4SYmHpMa-fyYzXQUYEoRadrIR0";
+      // Test implementation
+      const body = {
+        destination: "NABEUL",
+        userId,
+        seat: "SN-2025-06-26-22",
+      };
+
+      console.log("body:", body);
+
+      const response = await request(createServer().app)
+        .post(`${apiVersion}${endPoint.Reservation.ADD_RESERVATION}`)
+        .set("Cookie", `jwt=${token}`)
+        .send(body)
+
+        .expect(HttpStatusCode.BAD_REQUEST);
+
+      expect(response.body.error.message as string).toBe(
+        "This user have a seat"
+      );
     });
 
-    test("should throw an error when creating a reservation for a non-verified user", async () => {
+    test.skip("should throw an error when creating a reservation for a non-verified user", async () => {
       // Test PASSED
       let userId = "796c0c01-afae-4c63-84b0-272932dd7f82";
       let token =
@@ -107,10 +125,6 @@ describe("Test the Reservation Service", () => {
       expect(response.body.error.message as string).toBe(
         "This user is not verified"
       );
-    });
-
-    test("should throw an error when creating a reservation for a user with an existing reservation", async () => {
-      // Test implementation
     });
   });
 
